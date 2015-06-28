e1f8e77f4181f79c7ab00902e8cf26343eabc635
morganric <morganric@gmail.com>
2015-06-28 17:58:06 +0100

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include UrlHelper
+
+    layout 'sandstone'
 
   def after_sign_in_path_for(resource)
     case current_user.role
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
+     @user = User.find(params[:id])
+
+     
+    
+
     unless current_user.admin?
       unless @user == current_user
         redirect_to :back, :alert => "Access denied."
