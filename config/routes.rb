57bbc45ba96d7c4528c68ae839181e2b038d7784
morganric <morganric@gmail.com>
2015-07-08 17:25:25 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,38 @@
 Rails.application.routes.draw do
+
+
   resources :categories
   resources :projects
-  resources :profiles
+ resources :profiles
   get "content/silver"
   get "content/gold"
   get "content/platinum"
   mount Payola::Engine => '/payola', as: :payola
-  
   mount Upmin::Engine => '/admin'
 
   get 'products/:id', to: 'products#show', :as => :products
-  get ':id/tagged/:tag', to: 'profiles#tag', :as => :tag
-  get ':id/:category_id', to: 'profiles#category', :as => :user_category
+  
 
   devise_for :users, :controllers => { :registrations => 'registrations' }
   devise_scope :user do
     put 'change_plan', :to => 'registrations#change_plan'
   end
-  resources :users
 
  
+      resources :users
 
-  scope ":id" do
+   scope ":id" do
     get '', to: 'profiles#show', :as =>  :vanity_profile
   end
 
 
-  constraints(Subdomain) do  
+
+    constraints(Subdomain) do  
      get '/' => 'profiles#show', via: [:get, :post]
   end 
+
+  get ':id/tagged/:tag', to: 'profiles#tag', :as => :tag
+  get ':id/:category_id', to: 'profiles#category', :as => :user_category
 
 
   authenticated :user do
@@ -38,6 +42,7 @@ Rails.application.routes.draw do
   unauthenticated do
     root to: "visitors#index"
   end
+
 
 
 end
