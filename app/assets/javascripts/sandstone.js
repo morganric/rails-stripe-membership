a7a7043f1e4a2ef714035559adbbaeec042acb31
morganric <morganric@gmail.com>
2015-07-27 16:38:21 +0100

    working

--- app/assets/javascripts/sandstone.js
+++ app/assets/javascripts/sandstone.js
@@ -26,21 +26,21 @@
 
 
 
-  //   $('#masonry-container').imagesLoaded(
-  //    function(){
-  //      $('#masonry-container').masonry({
-   //  itemSelector: '.box',
-   //  // columnWidth: 300,
-  //     gutter: 15,
-   //  isAnimated: !Modernizr.csstransitions,
-   //    isFitWidth: true,
-   //  // set columnWidth a fraction of the container width
-   //  // columnWidth: function( containerWidth ) {
-   //  //   return containerWidth / 6;
+    // $('#masonry-container').imagesLoaded(
+    //  function(){
+    //    $('#masonry-container').masonry({
+    // itemSelector: '.box',
+    // // columnWidth: 300,
+    //   gutter: 15,
+    // isAnimated: !Modernizr.csstransitions,
+    //   isFitWidth: true
+    // // set columnWidth a fraction of the container width
+    // // columnWidth: function( containerWidth ) {
+    // //   return containerWidth / 6;
 
-   //  // }
+    // // }
     // });
-   //  });
+    // });
 
         var $container = $('#masonry-container');
 
@@ -62,31 +62,35 @@
 
 
 
-  $container.infinitescroll({
-    navSelector  : '.pagination',    // selector for the paged navigation 
-    nextSelector : '.pagination a',  // selector for the NEXT link (to page 2)
-    itemSelector : '.box',     // selector for all items you'll retrieve
-    loading: {
-        finishedMsg: 'No more pages to load.',
-        img: 'http://i.imgur.com/6RMhx.gif'
+    $('#masonry-container').infinitescroll({
+      navSelector  : '.pagination',    // selector for the paged navigation 
+      nextSelector : '.next_page a',  // selector for the NEXT link (to page 2)
+      itemSelector : '.box',     // selector for all items you'll retrieve
+      loading: {
+          finishedMsg: 'No more pages to load.',
+          img: 'http://i.imgur.com/6RMhx.gif'
+        }
+      },
+      // trigger Masonry as a callback
+      function( newElements ) {
+        // hide new items while they are loading
+        var $newElems = $( newElements ).css({ opacity: 0 });
+        // ensure that images load before adding to masonry layout
+
+
+        $newElems.imagesLoaded(function(){
+          // show elems now they're ready
+          $newElems.animate({ opacity: 1 });
+          $container.masonry( 'appended', $newElems, true ); 
+
+        });
       }
-    },
-    // trigger Masonry as a callback
-    function( newElements ) {
-      // hide new items while they are loading
-      var $newElems = $( newElements ).css({ opacity: 0 });
-      // ensure that images load before adding to masonry layout
+    );
 
-
-      $newElems.imagesLoaded(function(){
-        // show elems now they're ready
-        $newElems.animate({ opacity: 1 });
-        $container.masonry( 'appended', $newElems, true ); 
-
-      });
-    });
 
 
  
 
   });
+
+
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all.order('created_at DESC')
+    @projects = Project.all.order('created_at DESC').page(params[:page]).per(10)
     
   end
 
