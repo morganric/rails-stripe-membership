6f2b35a37766f58ca86f595b01ef7346bf85e895
morganric <morganric@gmail.com>
2015-08-03 17:23:41 +0100

    this

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
    before_action :allow_iframe
-   before_filter :authenticate_user!,  except: [:index, :show, :tag, :embed, :modal, :random, :popular]
+   before_filter :authenticate_user!,  except: [:index, :show, :tag, :embed, :modal, :random, :popular, :featured]
    
   # GET /projects
   # GET /projects.json
